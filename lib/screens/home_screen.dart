86ede8e9ad0d1d6e80ce71c097554e280657058a
ismontana <ismaotsss@gmail.com>
2024-02-20 13:41:46 -0600

    Finalizing the app theme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,50 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
-const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Componentes de flutter',
-        style: TextStyle(
-          color: Color.fromARGB(255, 255, 255, 255)
+        title: const Text(
+          'Componentes de flutter',
+          style: TextStyle(color: Colors.white),
         ),
-        ),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-            leading: const Icon(Icons.input_rounded),
-            title: Text('Entradas',
-            style: Theme.of(context).textTheme.headlineLarge,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.input_rounded,
+                            color: AppTheme.iconColor),
+            title: Text(
+              'Entradas',
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
-            subtitle: const Text('Diferentes widgets para entradas de flutter'
+            subtitle: Text(
+              'Diferentes widgets para entradas de flutter',
+              style: Theme.of(context).textTheme.bodySmall, // Aquí aplicamos el nuevo estilo bodySmall
             ),
-            trailing: const Icon(Icons.arrow_circle_right),
+            trailing: const Icon(Icons.arrow_circle_right,
+                                  color: AppTheme.iconColor),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.list_alt_outlined,
+                                color: AppTheme.iconColor),
+            title: Text(
+              'ListView.Builder',
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
-            const Divider(),
-            ListTile(
-            leading: const Icon(Icons.list_alt_outlined),
-            title: Text('ListView.Builder',
-            style: Theme.of(context).textTheme.headlineLarge,
-            ),      
-            subtitle: const Text('Scroll infinito',
+            subtitle: Text(
+              'Scroll infinito',
+              style: Theme.of(context).textTheme.bodySmall, // Aquí aplicamos el nuevo estilo bodySmall
             ),
-            trailing: const Icon(Icons.arrow_circle_right),
+            trailing: const Icon(Icons.arrow_circle_right,
+                                  color: AppTheme.iconColor),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.notification_add,
+                                color: AppTheme.iconColor),
+            title: Text(
+              'Notificaciones',
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
-            const Divider(),
-            ListTile(
-            leading: const Icon(Icons.notification_add),
-            title: Text('Notificaciones',
-            style: Theme.of(context).textTheme.headlineLarge,
+            subtitle: Text(
+              'Creación de notificaciones',
+              style: Theme.of(context).textTheme.bodySmall, // Aquí aplicamos el nuevo estilo bodySmall
             ),
-            subtitle: const Text('Creación de notificaciones'
-                      ),
-            trailing: const Icon(Icons.arrow_circle_right),
-            ),
-          ],
-        ),
-      );
+            trailing: const Icon(Icons.arrow_circle_right,
+                                  color: AppTheme.iconColor),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme{
-  // Declaraciones de color
+  // Color de iconos flechitas
+  static const iconColor = Color.fromARGB(255, 99, 17, 119);
+  // Declaraciones de color para titulos
   static const mainColor = Color.fromARGB(255, 85, 3, 74);
   static const backColor = Color.fromARGB(255, 225, 212, 255);
+  // Declaraciones para bodySmall
+  static const subtitleColor = Color.fromARGB(255, 114, 111, 113);
 
   // Declaraciones del tema claro
   static final ThemeData lightTheme = ThemeData.light().copyWith(
@@ -15,14 +19,20 @@ class AppTheme{
       headlineLarge: GoogleFonts.nunito(
         color: mainColor,
         fontSize: 16.0,
-        fontWeight: FontWeight.w400,
-        decoration: TextDecoration.underline,
+        fontWeight: FontWeight.w800,
+        decoration: TextDecoration.none,
         decorationColor: Colors.black,
         decorationStyle: TextDecorationStyle.dashed,
         decorationThickness: 3.0,
        // fontStyle: FontStyle.italic,
         ),
-       // bodySmall:
+      // Define el estilo para bodySmall
+      bodySmall: GoogleFonts.nunito(
+        color: subtitleColor,
+        fontSize: 12.0,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.w600,
       ),
+    ),
   );
 }
