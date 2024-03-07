3269c34220a3780ddcdda684616ddee6798dd5a7
ismontana <ismaotsss@gmail.com>
2024-03-07 12:02:59 -0600

    Functional ButtomNavigationBar

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/home_screen.dart';
+import 'package:practica3/screens/images_screen.dart';
+import 'package:practica3/screens/infinite_list_screen.dart';
+import 'package:practica3/screens/notifications_screen.dart';
 import 'package:practica3/theme/app_theme.dart';
 
 class InputsScreen extends StatefulWidget {
@@ -17,6 +21,33 @@ class _InputsScreenState extends State<InputsScreen> {
   double valueSlider = 0.0; // Inicia en 0
   int selectedIndex = 0;
   int selectedRadioOption = 0;
+  List screens = const [
+    HomeScreen(),
+    InfinitieListScreen(),
+    NotificationsScreen(),
+    ImagesScreen()
+  ];
+
+  openScreen(int index){
+    setState(() {
+      MaterialPageRoute ruta = 
+              MaterialPageRoute(builder: (context) => const HomeScreen());
+      switch ( index ){
+        case 0: ruta = MaterialPageRoute(builder: (context) => const HomeScreen());
+                break;
+        case 1: ruta = MaterialPageRoute(builder: (context) => const InfinitieListScreen());
+                break;
+        case 2: ruta = MaterialPageRoute(builder: (context) => const NotificationsScreen());
+                break;
+        case 3: ruta = MaterialPageRoute(builder: (context) => const ImagesScreen());
+                break;
+    }
+    selectedIndex = index;
+    print('selectedIndex = $selectedIndex');
+    Navigator.push(context, ruta);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,12 +76,19 @@ class _InputsScreenState extends State<InputsScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
-        backgroundColor: Color.fromARGB(255, 180, 167, 197),
+        backgroundColor: Color.fromARGB(255, 44, 43, 44),
+        unselectedItemColor: const Color.fromARGB(255, 167, 164, 164),
+        onTap: (index) => openScreen(index),
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.video_camera_back_outlined, 
+            icon: Icon(Icons.home_outlined, 
             color: AppTheme.negroColor),
-            label: "Home"
+            label: "Inicio"
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_outlined, 
+            color: AppTheme.negroColor),
+            label: "Lista"
             ),
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications_active_outlined,
@@ -58,9 +96,14 @@ class _InputsScreenState extends State<InputsScreen> {
             label: "Notificaciones"
             ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined,
+            icon: Icon(Icons.image_outlined,
             color: AppTheme.negroColor),
-            label: "Cuenta"
+            label: "Imagenes"
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.exit_to_app_outlined,
+            color: AppTheme.negroColor),
+            label: "Salida"
             ),
         ],
         unselectedLabelStyle: AppTheme.lightTheme.textTheme.bodyMedium,
